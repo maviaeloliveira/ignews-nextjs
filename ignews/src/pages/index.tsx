6cf0838b37eac32e5f36b0e42842c0217784ce60
maviaeloliveira <maviael.oliveira@gmail.com>
2022-08-18 20:48:46 -0300

    first commit

---
+++ ignews/src/pages/index.tsx
@@ -0,0 +1,27 @@
+
+import Head from '../../node_modules/next/head'
+import { SubscribeButton } from '../components/SubscribeButton/index'
+import styles from './home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications<br/>
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton/>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
+    </>
+
+  )
+}
