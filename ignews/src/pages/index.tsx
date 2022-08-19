7c4f688cab121c70faa3b18b79ad9d09efa61c78
maviaeloliveira <maviael.oliveira@gmail.com>
2022-08-18 21:24:27 -0300

    Usando SSR

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -2,8 +2,16 @@
 import Head from '../../node_modules/next/head'
 import { SubscribeButton } from '../components/SubscribeButton/index'
 import styles from './home.module.scss'
+import {GetServerSideProps} from 'next'
+import { stripe } from '../services/stripe'
 
-export default function Home() {
+interface HomeProps{
+  product:{
+    priceId:string;
+    amount: number;
+  }
+}
+export default function Home({product}:HomeProps) {
   return (
     <>
     <Head>
@@ -15,13 +23,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications<br/>
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding"/>
       </main>
     </>
 
   )
+}
+
+export const getServerSideProps:GetServerSideProps  = async () =>{
+  const price = await stripe.prices.retrieve('price_1LYIDqBV77V7KXtEoxzIg2qf')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style:'currency',
+      currency:'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return{
+    props:{
+      product
+    }
+    
+  }
 }
